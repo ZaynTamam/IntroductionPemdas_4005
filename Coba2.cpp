fab660118de6c5720f3c63ef02fdc092890cc438
ZaynTamam <tamam.zain78@gmail.com>
2026-02-23 11:58:27 +0700

    mengganti teks hasil ulangan

--- Coba2.cpp
+++ Coba2.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    cout << "Masukkan nilai ulangan 1: ";
+    cout << "Masukkan nilai ulangan a: ";
     cin >> a;
-    cout << "Masukkan nilai ulangan 2: ";
+    cout << "Masukkan nilai ulangan b: ";
     cin >> b;
-    cout << "Masukkan nilai ulangan 3: ";
+    cout << "Masukkan nilai ulangan c: ";
     cin >> c;
 
     if (a > b && a > c) {
-        cout << "Nilai terbesar adalah: " << a << endl;
+        cout << "Nilai terbesar ulangan adalah: " << a << endl;
     } else if (b > a && b > c) {
-        cout << "Nilai terbesar adalah: " << b << endl;
+        cout << "Nilai terbesar ulangan adalah: " << b << endl;
     } else {
-        cout << "Nilai terbesar adalah: " << c << endl;
+        cout << "Nilai terbesar ulangan adalah: " << c << endl;
     }
 
     return 0;
