da18e7b2eaf984a8a434c53690d722cb1284f451
ZaynTamam <tamam.zain78@gmail.com>
2026-02-23 12:12:29 +0700

    Menentukan luas dari panjang dan lebar

--- Coba2.cpp
+++ Coba2.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    cout << "Masukkan nilai ulangan a: ";
+    cout << "Masukkan nilai ulangan 1: ";
     cin >> a;
-    cout << "Masukkan nilai ulangan b: ";
+    cout << "Masukkan nilai ulangan 2: ";
     cin >> b;
-    cout << "Masukkan nilai ulangan c: ";
+    cout << "Masukkan nilai ulangan 3: ";
     cin >> c;
 
     if (a > b && a > c) {
-        cout << "Nilai terbesar ulangan adalah: " << a << endl;
+        cout << "Nilai terbesar adalah: " << a << endl;
     } else if (b > a && b > c) {
-        cout << "Nilai terbesar ulangan adalah: " << b << endl;
+        cout << "Nilai terbesar adalah: " << b << endl;
     } else {
-        cout << "Nilai terbesar ulangan adalah: " << c << endl;
+        cout << "Nilai terbesar adalah: " << c << endl;
     }
 
     return 0;
--- ccoba.cpp
+++ ccoba.cpp
@@ -1,7 +1,20 @@
-#include
+#include <iostream>
+
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main()
+//langkah 1: deklarasi variabel
+{ int panjang, lebar, luas;    //langkah 2: input nilai panjang
+    cout << "Masukkan panjang: ";
+    cin >> panjang;
+
+    //langkah 3: input nilai lebar
+    cout << "Masukkan lebar: ";
+    cin >> lebar;
+
+    //langkah 4: proses perhitungan luas
+    luas = panjang * lebar;
+
+//langkah 5: output hasil perhitungan luas
+    cout << "hasilnya adalah: " << luas << endl;
 }
